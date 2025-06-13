407b1405cb27ffdb0348e31d2c7c5bdd04870e63
suchetana <122291721+suchizz@users.noreply.github.com>
2025-06-13 14:58:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ if uploaded_files:
             data = {"question": question}
 
             # Send to backend
-            response = requests.post("https://5def-35-232-144-248.ngrok-free.app/analyze", files=files, data=data)
+            response = requests.post("https://5def-35-232-144-248.ngrok-free.app/analyze", files=files, json=data)
 
             try:
                 result = response.json()
